ef905f06075c882b920ca77eb466a947b11bf3fe
khanahmedm <khan.ahmed.m@outlook.com>
2025-10-23 15:04:18 -0500

    holds fixtures used in multiple tests

---
+++ tests/loaders/conftest.py
@@ -0,0 +1,13 @@
+# tests/conftest.py
+import pytest
+from unittest.mock import MagicMock
+
+@pytest.fixture
+def mock_spark():
+    spark = MagicMock()
+    spark.read.options.return_value.format.return_value.load.return_value.count.return_value = 3
+    return spark
+
+@pytest.fixture
+def mock_logger():
+    return MagicMock()
